038e6a23efbb645a2e3ddf368e022cdd31ffe8e1
Ron Chen <ron.chen@hamaarag.org.il>
2020-12-06 14:04:33 +0200

    fixed bugs in vars_left name

--- R/GetTotalAbundanceAndRichness.R
+++ R/GetTotalAbundanceAndRichness.R
@@ -5,9 +5,9 @@ GetTotalAbundanceAndRichness <- function(Tlong,vars_left,var_right = "species_la
   pacman::p_load(tidyvers)
   
   # cast into wide format and calculate abundance and richness ----
-  Twide <- cast(Tlong,paste0(paste0(vars,collapse = " + ")," ~ ",var_right), fun.aggregate = sum, value = var_value) %>% 
+  Twide <- cast(Tlong,paste0(paste0(vars_left,collapse = " + ")," ~ ",var_right), fun.aggregate = sum, value = var_value) %>% 
     as_tibble()
-  spec_ind <- seq(length(vars)+1,ncol(Twide))
+  spec_ind <- seq(length(vars_left)+1,ncol(Twide))
   Twide1 <- mutate(Twide, total_abund = rowSums(Twide[,spec_ind]))
   Twide2 <-  mutate(Twide1,total_richness = rowSums(Twide[,spec_ind]>0))
   return(Twide2)
