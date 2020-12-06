b234eab534204239c1438ffcc44d0fefbffee00f
Ron Chen <ron.chen@hamaarag.org.il>
2020-12-06 13:34:37 +0200

    added a command for loading required library

--- R/GetTotalAbundanceAndRichness.R
+++ R/GetTotalAbundanceAndRichness.R
@@ -1,5 +1,10 @@
 GetTotalAbundanceAndRichness <- function(Tlong,vars_left,var_right = "species_latin",var_value = "Total") {
-  library(dplyr)
+  # Install required packages -----------------------------------------------
+  
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(tidyvers)
+  
+  # cast into wide format and calculate abundance and richness ----
   Twide <- cast(Tlong,paste0(paste0(vars,collapse = " + ")," ~ ",var_right), fun.aggregate = sum, value = var_value) %>% 
     as_tibble()
   spec_ind <- seq(length(vars)+1,ncol(Twide))
